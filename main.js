a7ca6ae9b72db874610f72abcdbbb2ef15f1ae23
Matt Bechtel <matt.b.bechtel@gmail.com>
2014-05-10 08:46:52 -0700

    board now draws dynamic sizes, squares given unique ids

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 $( document ).ready( function(){
-  var gridSize = $('.gol-row').length;
-  $( '.gol-row' ).each( function( index, element ){
-    for (var i = 0; i<gridSize; i++){
-      $(element).append("<div class='gol-square dead'></div>")
-      console.log("index is: " + index + ", col is: " + i)
-    };
-  });
+  View.drawBoard(6)
 })
+
+
+
+var View = {
+  drawBoard: function(gridSize){
+    for (var i = 0; i < gridSize; i++) {
+      $('#board-container').append("<div class='gol-row' id='row" + i + "'></div>")
+      for (var j = 0; j<gridSize; j++){
+        $('#row' + i).append("<div class='gol-square dead' id='square" + (i*gridSize + j) +"''></div>")
+        console.log("adding to #row"+i)
+      };
+    };
+  }
+}
