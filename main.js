8e370670a4bfd1f6b420063b52d15474784622bf
Matt Bechtel <matt.b.bechtel@gmail.com>
2014-05-10 09:15:24 -0700

    Testing out setting squares to alive/dead

--- main.js
+++ main.js
@@ -1,17 +1,27 @@
 $( document ).ready( function(){
-  View.drawBoard(6)
+  testGridSize = 5
+  testArray = Array(testGridSize^2)
+  for (var i = 0; i < testArray.length; i++){
+    testArray[i] = 0;
+  }
+  testArray[7], testArray[11], testArray[12], testArray[13], testArray[17] = 1
+  console.log(testArray)
+  View.drawBoard(testGridSize, testArray)
 })
 
-
-
 var View = {
-  drawBoard: function(gridSize){
+  drawBoard: function(gridSize, stateArray){
     for (var i = 0; i < gridSize; i++) {
       $('#board-container').append("<div class='gol-row' id='row" + i + "'></div>")
       for (var j = 0; j<gridSize; j++){
-        $('#row' + i).append("<div class='gol-square dead' id='square" + (i*gridSize + j) +"''></div>")
-        console.log("adding to #row"+i)
+        squareId = i * gridSize + j
+        squareState = "dead"
+        if (stateArray[squareId] === 1) {
+          squareState = "alive"
+        };
+        $('#row' + i).append("<div class='gol-square " + squareState + "' id='square" + squareId +"''></div>")
       };
     };
   }
 }
+
