fdffd8403aab1940dc689c8fa6ab2032eccb35da
Matt Bechtel <matt.b.bechtel@gmail.com>
2014-10-15 20:12:42 -0700

    Fix errors in controller found by JSHint

--- controller.js
+++ controller.js
@@ -1,25 +1,25 @@
 $( document ).ready( function(){
   Board.zero();
-  Board.setAlive([1,2])
-  Board.setAlive([2,2])
-  Board.setAlive([3,2])
-  View.drawBoard(Board)
-})
+  Board.setAlive([1,2]);
+  Board.setAlive([2,2]);
+  Board.setAlive([3,2]);
+  View.drawBoard(Board);
+});
 
 var View = {
   drawBoard: function(board){
     for (var i = 0; i < board.width; i++) {
-      $('#board-container').append("<div class='gol-row' id='row" + i + "'></div>")
+      $('#board-container').append("<div class='gol-row' id='row" + i + "'></div>");
       for (var j = 0; j<board.height; j++){
-        squareId = i * board.width + j
-        squareState = "dead"
+        squareId = i * board.width + j;
+        squareState = "dead";
         if (board.state[squareId] === 1) {
-          squareState = "alive"
-        };
-        $('#row' + i).append("<div class='gol-square " + squareState + "' id='square" + squareId +"''></div>")
-      };
-    };
+          squareState = "alive";
+        }
+        $('#row' + i).append("<div class='gol-square " + squareState + "' id='square" + squareId +"''></div>");
+      }
+    }
   }
-}
+};
 
 
