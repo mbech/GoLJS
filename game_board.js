86e01361aba86ebbf816810c01e4583bcf6bc216
Matt Bechtel <matt.b.bechtel@gmail.com>
2014-10-15 22:32:39 -0700

    Fix handling of certain cell states

--- game_board.js
+++ game_board.js
@@ -81,15 +81,18 @@ board.nextCellState = function (cell_loc){
     neighbors[7] = 1;
   }
 
-  console.log(neighbors)
-
   var total_live_neighbors = neighbors.reduce(function(a, b) { return a + b; });
   //Rules are as follows for next state:
   //Any live cell with fewer than two live neighbours dies, as if caused by under-population.
   //Any live cell with more than three live neighbours dies, as if by overcrowding.
-  if (total_live_neighbors < 2 || total_live_neighbors > 3){return 0;}
   //Any live cell with two or three live neighbours lives on to the next generation.
-  if(this.getState(cell_loc) === 1 && total_live_neighbors === 2){return 1;}
   //Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+  if(this.getState(cell_loc) === 1){
+    if(total_live_neighbors === 2 || total_live_neighbors === 3){
+      return 1;
+    }
+  }
   if(this.getState(cell_loc) === 0 && total_live_neighbors === 3){return 1;}
+  //if (total_live_neighbors < 2 || total_live_neighbors > 3){return 0;}
+  return 0;
 }; 
