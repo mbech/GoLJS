d243fb753f15491955325554dc5494d1e829ab67
Matt Bechtel <matt.b.bechtel@gmail.com>
2014-10-18 19:07:03 -0700

    Move view code (board redraw) out of controller into separate file

---
+++ view.js
@@ -0,0 +1,19 @@
+var view = {};
+
+view.drawboard = function(board){
+  $('#board-container').empty();
+  for (var i = 0; i < board.height; i++) {
+    $('#board-container').append("<div class='gol-row' id='row" + i + "'></div>");
+    for (var j = 0; j<board.width; j++){
+      squareId = i * board.width+ j;
+      squareState = "dead";
+      if (board.state[squareId] === 1) {
+        squareState = "alive";
+      }
+      $('#row' + i).append("<div class='gol-square " + squareState + "' id='square" + squareId +"''></div>");
+    }
+  }
+};
+
+
+
