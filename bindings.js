0fecb44e9a97b4ee2cbe2cf6a8e78c71c0d9f2ba
Matt Bechtel <matt.b.bechtel@gmail.com>
2014-10-20 21:07:36 -0700

    Make cells clickable to set alive/dead, drawboard renaming

--- bindings.js
+++ bindings.js
@@ -1,17 +1,17 @@
 var initialBindings = function(){
   $('#advance-board').on('click', function(){
     board.nextState();
-    view.drawboard(board);
+    view.drawBoard(board);
   });
 
   $('#reset-board').on('click', function(){
     board.zero();
-    view.drawboard(board);
+    view.drawBoard(board);
   });
 
   $('#randomize-board').on('click', function(){
     board.populateRandom();
-    view.drawboard(board);
+    view.drawBoard(board);
   });
 
   $('#automate-board').on('click', function(){
@@ -23,6 +23,17 @@ var initialBindings = function(){
       automate.enable();
       automate.advance();
     }
+  });
+
+  $( document ).on('click', '.gol-square', function(){
+    var $clicked_cell = $(this);
+    var cell_loc = $clicked_cell.data("loc");
+    if ($clicked_cell.hasClass("alive")){
+      board.setDead(cell_loc);
+    }else{
+      board.setAlive(cell_loc);
+    }
+    view.drawBoard(board); 
   });
 
 };
