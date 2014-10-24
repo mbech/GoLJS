a4757c3753fdbfa86d0ce4f360a6aaca1322791d
Matt Bechtel <matt.b.bechtel@gmail.com>
2014-10-23 21:15:17 -0700

    Add repeating demo pattern on page load

--- js/controller.js
+++ js/controller.js
@@ -1,10 +1,6 @@
 $( document ).ready( function(){
   board.zero();
-  board.setAlive([1,3]);
-  board.setAlive([2,3]);
-  board.setAlive([3,3]);
-  board.setAlive([3,2]);
-  board.setAlive([2,1]);
+  board.populateDemo();
   view.refresh(board);
   initialBindings();
 });
